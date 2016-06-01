b76b074b0d0cbcf04a0ba378418d36d41d775a50
Steve Pentler <stevepentler@gmail.com>
2016-06-01 13:22:52 -0500

    Creates and renders homepage component

--- app/assets/javascripts/components/_home-page.js.jsx
+++ app/assets/javascripts/components/_home-page.js.jsx
@@ -2,30 +2,30 @@ var HomePage = React.createClass({
   render() {
     return (
       <div>
-  <div className="anton">
-    <div className="row center opaque white-text">
-      <div className="col s12 m5 l5 offset-m6 offset-l6">
-        <h1 className="titillium">PAINCAVER</h1>
-        <a href="/auth/strava" className="btn"> Login with Strava</a>
-        <p id='home-description'>Calculate your trail's difficulty rating using:</p>
-        <div className="row center white-text">
-          <div className="col s12 m4 l4">
-            <i className="medium material-icons">navigation</i>
-            <h5>elevation</h5>
-          </div>
-          <div className="col s12 m4 l4">
-            <i className="medium material-icons">trending_up</i>
-             <h5>heartrate</h5>
-          </div>
-          <div className="col s12 m4 l4">
-            <i className="medium material-icons">query_builder</i>
-             <h5>duration</h5>
+        <div className="anton">
+          <div className="row center opaque white-text">
+            <div className="col s12 m5 l5 offset-m6 offset-l6">
+              <h1 className="titillium">PAINCAVER</h1>
+              <a href="/auth/strava" className="btn"> Login with Strava</a>
+              <p id='home-description'>Calculate your trail's difficulty rating using:</p>
+              <div className="row center white-text">
+                <div className="col s12 m4 l4">
+                  <i className="medium material-icons">navigation</i>
+                  <h5>elevation</h5>
+                </div>
+                <div className="col s12 m4 l4">
+                  <i className="medium material-icons">trending_up</i>
+                   <h5>heartrate</h5>
+                </div>
+                <div className="col s12 m4 l4">
+                  <i className="medium material-icons">query_builder</i>
+                   <h5>duration</h5>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-      </div>
     )
   }
-})
+});
