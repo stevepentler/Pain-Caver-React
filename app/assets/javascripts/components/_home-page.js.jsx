2a901f9de54f951872d8c5a055f11f2f9568a207
Steve Pentler <stevepentler@gmail.com>
2016-06-01 13:20:33 -0500

    Styles homepage

--- app/assets/javascripts/components/_home-page.js.jsx
+++ app/assets/javascripts/components/_home-page.js.jsx
@@ -1,4 +1,4 @@
-var Main = React.createClass({
+var HomePage = React.createClass({
   render() {
     return (
       <div>
@@ -28,4 +28,4 @@ var Main = React.createClass({
       </div>
     )
   }
-});
+})
